849169a5842283de21bc9379855f209d7320b008
chen0423 <chenziyuan0423@gmail.com>
2016-08-21 00:49:57 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #! /bin/sh
 # install vimrc
+echo "This script will delete all file in ~/.vim/ and ~/.vimrc, are you sure to continue?(y/N)"
+read ans
+if [ "$ans" = "n" ] || [ "$ans" = "N" ] || [ -z "$ans" ]; then
+    echo "User declined, exiting......"
+    exit 0
+fi
+if [ -d "$HOME/.vim" ]; then
+    echo "deleting ~/.vim"
+    rm -rf $HOME/.vim
+fi
+if [ -f "$HOME/.vimrc" ]; then
+    echo "deleting ~/.vimrc"
+    rm -rf
+fi
+echo "Now fetching and install new vim configuration"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vimrc .vimrc
 vim +PluginInstall +qall
+echo "Finish vim coniguration!"
