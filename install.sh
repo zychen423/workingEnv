81134f42d068e79c624d28cab4e08441b7a91ed3
Zi-Yuan Chen <chenziyuan0423@gmail.com>
2021-09-22 04:30:27 +0000

    <chore> update all

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 # install vimrc
 echo "This script will delete all file in ~/.vim/ and ~/.vimrc, are you sure to continue?(y/N)"
 read ans
@@ -14,6 +14,9 @@ if [ -f "$HOME/.vimrc" ]; then
     echo "deleting ~/.vimrc"
     rm -rf $HOME/.vimrc
 fi
+
+echo "Install zsh"
+sudo apt-get install -y zsh 
 echo "Now fetching and install new vim configuration"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vimrc ~/.vimrc
@@ -22,9 +25,18 @@ echo "Finish vim coniguration!"
 
 echo "Installing oh-my-zsh"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Intall oh-my-zsh plugin"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+
+
+
 echo "setting up ~/.zshrc"
-cp ./.zshrc ~/.zshrc
-cp ./.tmux.conf ~/.tmux.conf
+cp ./zshrc ~/.zshrc
+cp ./tmux.conf ~/.tmux.conf
 
 
 echo "Finish!!"
